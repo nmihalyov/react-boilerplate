809650d7efb30615a2d255eded10d1dbf67944d1
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2019-04-01 13:39:38 +0300

    Move Redux Provider to render function

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import './style.sass';
-
-import store from '../../store';
 
 class App extends React.Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<main className="content"></main>
-			</Provider>
+			<main className="content"></main>
 		);
 	}
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 import './index.sass';
 
+import store from 'store';
+
 import App from './components/App';
 
-ReactDOM.render(<App/>,	document.querySelector('.page-wrapper'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>,
+	document.querySelector('.page-wrapper')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
